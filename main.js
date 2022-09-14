94892d742dad6ec5a67422149d4625e53c3ea295
Zero-Junker <112006320+Zero-Junker@users.noreply.github.com>
2022-09-14 14:51:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//quetion 1 & 2
+    const myNumber = 5;
+//quetion 3
+    const myOtherNumber = 5;
+//quetion 4
+    let myTotal;
+    myTotal = myNumber + myOtherNumber;
+//quetion 5
+    const myName = "Zero";
+    let myCombo = "myNumber + myOtherNumber";
+//quetion 6
+    let myOtherCombo = myNumber + myOtherNumber;
+//quetion 7
+    let myRemainder = myNumber % 5;
+//quetion 8
+    const i = 2;
+//quetion 9
+    const int = 4;
+//quetion 10
+     let x = false;
+     Boolean(x);
